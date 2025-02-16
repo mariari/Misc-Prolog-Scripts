f2c70c69804fd23372edfac9cdc310862cec6215
mariari <mariari@protonmail.com>
2025-02-16 16:55:29 +0800

    Refactored the set interface to have 4 functions
    This covers what is in Cooks paper, I could make more examples, but it
    shows the point I was trying to make

--- oo-set.pl
+++ oo-set.pl
@@ -1,29 +1,40 @@
 :- use_module(library(logtalk)).
 
 :- protocol(set).
-   :- public([member/1]).
+   :- public([member/1, empty/0, insert/2, union/2]).
 :- end_protocol.
 
 :- object(empty_set, implements(set)).
-   member(_) :- false.
+   empty.
+   %% This is not needed false by default
+   member(false).
+   insert(X, Set) :- Set = single(X).
+   union(Set1, SetUnion) :- Set1 = SetUnion.
 :- end_object.
 
-:- object(evens, implements(set)).
-   :- use_module(library(clpfd)).
-   member(X) :- X mod 2 #= 0.
-:- end_object.
+:- category(default_set_behaviour).
+   insert(X, Set) :-
+       self(Self), Set = union(Self, single(X)).
+   union(Set1, SetUnion) :-
+       self(Self), SetUnion = union(Self, Set1).
+:- end_category.
 
-:- object(odds, implements(set)).
-   :- use_module(library(clpfd)).
-   member(X) :- X mod 2 #= 1.
-:- end_object.
-
-:- object(single(_S1_), implements(set)).
+:- object(single(_S1_), imports(default_set_behaviour), implements(set)).
    :- use_module(library(clpfd)).
    member(X) :- X #= _S1_.
 :- end_object.
 
+:- object(evens, imports(default_set_behaviour), implements(set)).
+   :- use_module(library(clpfd)).
+   member(X) :- X mod 2 #= 0.
+:- end_object.
+
+:- object(odds, imports(default_set_behaviour), implements(set)).
+   :- use_module(library(clpfd)).
+   member(X) :- X mod 2 #= 1.
+:- end_object.
+
 :- object(union(_S1_, _S2_), implements(set)).
-   member(X) :- _S1_::member(X).
-   member(X) :- _S2_::member(X).
+   member(X) :- implements_protocol(_S1_, set), _S1_::member(X).
+   member(X) :- implements_protocol(_S2_, set), _S2_::member(X).
 :- end_object.
