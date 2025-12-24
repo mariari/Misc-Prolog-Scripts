777a311221fa87ece7acb981295168a2308e4719
mariari <mariari@protonmail.ch>
2025-12-25 01:18:23 +0800

    Minor improvements on readability of Day1

--- AOC 2025/day1/day1.pl
+++ AOC 2025/day1/day1.pl
@@ -13,30 +13,35 @@ move_fun(Start, Result) --> "R", integer(X), {Result #= (Start + X) mod 100}.
 move(move(l, X)) --> "L", integer(X).
 move(move(r, X)) --> "R", integer(X).
 
-apply_move(move(l, X), Start, Result) :- Result #= (Start - X).
-apply_move(move(r, X), Start, Result) :- Result #= (Start + X).
+apply_move(Move, Start, Dial, DialFull) :-
+    delta(Move, D),
+    DialFull #= Start + D,
+    Dial #= DialFull mod 100.
 
-dial_protocol_1(X, N, M) :- 0 #=  X mod 100, M #= N + 1.
-dial_protocol_1(X, N, N) :- 0 #\= X mod 100.
+delta(move(l,X), -X).
+delta(move(r,X),  X).
 
-dial_protocol_2(X, Original_X, N, M) :-
-    Div #= abs(X // 100),
+dial_protocol_1(X, N, M) :-
+    0 #= X mod 100 #<==> B,
+    M #= N + B.
 
-    (X #=< 0)          #<==> Bneg,
-    (Original_X #\= 0) #<==> Bnz,
+dial_protocol_2(Full_Dial, Old_Normalized_Dial, Count, New_Count) :-
+    Div #= abs(Full_Dial // 100),
+
+    (Full_Dial #=< 0)           #<==> Bneg,
+    (Old_Normalized_Dial #\= 0) #<==> Bnz,
     Extra #= Bneg * Bnz,        % True iff bneg ∧ bnz
 
-    M #= N + Div + Extra.
+    New_Count #= Count + Div + Extra.
 
 answer_part1(Input, (Old_Dial, Old_Count), (Dial, Count)) :-
     phrase(move(M), Input),
-    apply_move(M, Old_Dial, Dial),
+    apply_move(M, Old_Dial, Dial, _),
     dial_protocol_1(Dial, Old_Count, Count).
 
 answer_part2(Input, (Old_Dial, Old_Count), (Dial, Count)) :-
     phrase(move(M), Input),
-    apply_move(M, Old_Dial, Dial_Full),
-    Dial #= Dial_Full mod 100,
+    apply_move(M, Old_Dial, Dial, Dial_Full),
     dial_protocol_2(Dial_Full, Old_Dial, Old_Count, Count).
 
 solve_day1(part1, Input, Answer) :-
@@ -46,7 +51,7 @@ solve_day1(part2, Input, Answer) :-
 
 input_test([`L68`, `L30`, `R48`, `L5`, `R60`, `L55`, `L1`, `L99`, `R14`, `L82`]).
 
-% open('./AOC 2025/day1/input', read, _Str), aoc_common:read_file(_Str, _Input), solve_day1(_, _Input, Answer).
+% open('./AOC 2025/day1/input', read, _Str), read_file(_Str, _Input), solve_day1(_, _Input, Answer).
 % Answer = 1195;
 % Answer = 6770.
 
