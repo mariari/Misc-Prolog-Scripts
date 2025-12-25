3a137a45edb19382b4dfd3efe85e42c665fe0337
mariari <mariari@protonmail.ch>
2025-12-25 19:37:40 +0800

    Moved around the utility functions to a shared file, some cleanup

--- AOC 2025/aoc_common.pl
+++ AOC 2025/aoc_common.pl
@@ -1,5 +1,7 @@
-:- module(aoc_common, [read_file/2]).
+:- module(aoc_common, [read_file/2, replicate_list/3, repeated/2]).
 :- autoload(library(readutil), [read_line_to_codes/2]).
+:- autoload(library(apply), [maplist/2]).
+:- autoload(library(lists), [append/2]).
 
 read_file(Stream,[]) :-
     at_end_of_stream(Stream).
@@ -7,3 +9,18 @@ read_file(Stream,[X|L]) :-
     \+ at_end_of_stream(Stream),
     read_line_to_codes(Stream,X),
     read_file(Stream,L).
+
+% Same as split for N = 2. 2x slower than day2:split
+replicate_list(L1, N, LRepeat) :-
+    length(LLRepeat, N),
+    maplist(=(L1), LLRepeat),
+    append(LLRepeat, LRepeat).
+
+% Version of replicate_list, for just recognization. Recognization
+% speeds up solve_day2(part2,_,_) from 16 seconds to 4 seconds.
+repeated(N, Codes) :-
+    length(Codes, L),
+    0 is L mod N, BlockLen is L // N,
+    length(Block, BlockLen), length(Blocks, N),
+    maplist(=(Block), Blocks),
+    append(Blocks, Codes).
--- AOC 2025/day2/day2.pl
+++ AOC 2025/day2/day2.pl
@@ -1,9 +1,10 @@
-:- module(day2, [solve_day2/3]).
+:- module(day2, [solve_day2/3, parse_input/3]).
 
+:- use_module(aoc(aoc_common)).
 :- autoload(library(dcg/basics), [integer/3]).
 :- autoload(library(dcg/high_order), [sequence/5]).
-:- autoload(library(lists), [append/3, append/2]).
-:- autoload(library(apply), [maplist/3, maplist/2]).
+:- autoload(library(lists), [append/3]).
+:- autoload(library(apply), [maplist/3]).
 :- autoload(library(backcomp), [sumlist/2]).
 :- autoload(library(aggregate), [aggregate_all/3]).
 
@@ -19,29 +20,12 @@ parse_input_bad([(R1, R2)]) --> range((R1, R2)).
 parse_input_bad([(R1, R2) | Ranges]) -->
     range((R1, R2)), ",", parse_input_bad(Ranges).
 
-% Answer written for part1
+% Answer written for part1, generalized in replicate_list, in common.
 split(L, FirstHalf, SecondHalf) :-
     length(L, N),
     Half is N div 2,
     length(FirstHalf, Half), length(SecondHalf, Half),
     append(FirstHalf, SecondHalf, L).
-
-% Same as split for N = 2. 2x slower than split
-replicate_list(L1, N, LRepeat) :-
-    length(LLRepeat, N),
-    maplist(=(L1), LLRepeat),
-    append(LLRepeat, LRepeat).
-
-% Version of replicate_list, for just checking. Avoids allocating
-% intermediate lists. Main benefit is that this just checks so we
-% speed up from 16 seconds to 4 seconds.
-% GPT gave this suggestion and code for speeding up things
-repeated(N, Codes) :-
-    length(Codes, L),
-    0 is L mod N, BlockLen is L // N,
-    length(Block, BlockLen), length(Blocks, N),
-    maplist(=(Block), Blocks),
-    append(Blocks, Codes).
 
 repeat_pattern(R1, R2, Number) :-
     between(R1, R2, Number),
@@ -53,22 +37,22 @@ repeat_patterns(R1, R2, Number) :-
     between(R1, R2, Number),
     number_codes(Number, CodeList),
     length(CodeList, L),
+    % Once makes it so we don't find another N such that this holds,
+    % causing an issue
     once(
         (between(2, L, N),
-        % replicate_list(_, N, CodeList)
-        repeated(N, CodeList)
-        )).
+         repeated(N, CodeList))). % replicate_list(_, N, CodeList)
+
+% Allocates a list, thus is slower, keeping for learning
+answer_part1_slow((R1, R2), N) :-
+    findall(X, repeat_pattern(R1, R2, X), Bag),
+    sumlist(Bag, N).
 
 answer_part1((R1, R2), N) :-
     aggregate_all(sum(X), repeat_pattern(R1, R2, X), N).
 
 answer_part2((R1, R2), N) :-
     aggregate_all(sum(X), repeat_patterns(R1, R2, X), N).
-
-% Allocates a list, thus is slower, keeping for learning
-answer_part1_slow((R1, R2), N) :-
-    findall(X, repeat_pattern(R1, R2, X), Bag),
-    sumlist(Bag, N).
 
 solve_day2(part1, Input, Answer) :-
     maplist(answer_part1, Input, New),
@@ -78,21 +62,17 @@ solve_day2(part2, Input, Answer) :-
     maplist(answer_part2, Input, New),
     sumlist(New, Answer).
 
-% First Run with the speed
 % ?- phrase_from_file(day2:parse_input(_Ranges), './AOC 2025/day2/input'),
 %    time(solve_day2(_, _Ranges, X)).
-% % 28,994,016 inferences, 1.027 CPU in 1.029 seconds (100% CPU, 28237734 Lips)
 % X = 37314786486 ;
-% % 732,337,010 inferences, 16.682 CPU in 16.716 seconds (100% CPU, 43899898 Lips)
 % X = 47477053982.
 
+% Speed of first run
+% % 28,994,016 inferences, 1.027 CPU in 1.029 seconds (100% CPU, 28237734 Lips)
+% % 732,337,010 inferences, 16.682 CPU in 16.716 seconds (100% CPU, 43899898 Lips)
 % After some optimizations
-% ?- phrase_from_file(day2:parse_input(_Ranges), './AOC 2025/day2/input'),
-%    time(solve_day2(_, _Ranges, X)).
 % % 40,630,475 inferences, 1.114 CPU in 1.116 seconds (100% CPU, 36463751 Lips)
-% X = 37314786486 ;
 % % 175,708,050 inferences, 4.427 CPU in 4.435 seconds (100% CPU, 39688094 Lips)
-% X = 47477053982 .
 
 % phrase_from_file(day2:parse_input(Ranges), './AOC 2025/day2/test').
 % Can run in reverse
