b795d6678a87749091d713cdc81f5ef6f9f00200
mariari <mariari@protonmail.ch>
2025-12-28 00:37:54 +0800

    Added back explicit ugraph relations
    The loss in performance wasn't worth it, I've already cached it, so
    we get back all the time

--- AOC 2025/day4/day4.pl
+++ AOC 2025/day4/day4.pl
@@ -1,15 +1,15 @@
 :- module(day4, [solve_day4/3, build_maze/2]).
 
-% Project a vector space into a ugraph like relation. Done lazily, so
-% none is officially allocated.
+% Project a vector space into a ugraph like relation.
 
 convert(64, 1). % @
 convert(46, 0). % .
 paper(64).
 
 % Could turn this into a fact to query but it isn't worth it.
-build_maze(Puzzle, (Ht, Keys)) :-
-    ht_new(Ht), build_puzzle_table(Puzzle, Ht), ht_keys(Ht, Keys).
+build_maze(Puzzle, (Ht, KeysRel)) :-
+    ht_new(Ht), build_puzzle_table(Puzzle, Ht), ht_keys(Ht, Keys),
+    maplist(neighboring_points, Keys, KeysRel).
 
 % Kinda an ugly way to grow this
 build_puzzle_table(Puzzle, Ht) :-
@@ -21,9 +21,8 @@ build_puzzle_table(Puzzle, Ht) :-
                 , P, 0, _))
          , Puzzle, 0, _).
 
-% table is too expensive as the predicate is cheap :- table neighboring_points/2.
 % This was a ugraph point-rels, but simplified to remove eager comp.
-neighboring_points((I,J), Rels) :-
+neighboring_points((I,J), ((I,J)-Rels)) :-
     Rels0 = [(I-1,J-1), (I,J-1), (I+1,J-1),
              (I-1,J)  ,          (I+1,J),
              (I-1,J+1), (I,J+1), (I+1,J+1)],
@@ -34,8 +33,7 @@ neighboring_points((I,J), Rels) :-
 lookup_plane(Table, Index, Value) :-
     (ht_get(Table, Index, Value) -> true; Value = 46). % Hacky default but works
 
-forkable(Table, Roll) :-
-    neighboring_points(Roll, Neighbors),
+forkable(Table, (Roll-Neighbors)) :-
     maplist(lookup_plane(Table), Neighbors, Numbers),
     maplist(convert, Numbers, Converted),
     lookup_plane(Table, Roll, Code),
@@ -43,8 +41,7 @@ forkable(Table, Roll) :-
     sum_list(Converted, Sum), Sum < 4.
 
 forkable_roll((Table, Keys), Roll) :-
-    member(Roll, Keys),
-    forkable(Table, Roll).
+    member(Roll, Keys), forkable(Table, Roll).
 
 all_forkable_rolls(Maze, Forked, Length) :-
     findall(R, forkable_roll(Maze, R), Forked), length(Forked, Length).
@@ -61,7 +58,7 @@ remove_until_stable(Maze, Total) :-
     ).
 
 remove_forked((Table, _), Forked) :-
-    maplist({Table}/[Ele]>>(ht_put(Table, Ele, 46)), Forked).
+    maplist({Table}/[(Ele-_)]>>(ht_put(Table, Ele, 46)), Forked).
 
 solve_day4(part1, Maze, Answer) :- all_forkable_rolls(Maze, _, Answer).
 solve_day4(part2, Maze, Answer) :- remove_until_stable(Maze, Answer).
@@ -73,15 +70,13 @@ solve_day4(part2, Maze, Answer) :- remove_until_stable(Maze, Answer).
 % Res = 1578 ;
 % Res = 10132 .
 % Time when using ugraph eagerly
-% % 1,135,450 inferences, 0.098 CPU in 0.098 seconds (100% CPU, 11641476 Lips)
-% % 2,446,404 inferences, 0.152 CPU in 0.153 seconds (100% CPU, 16050181 Lips)
-% % 141,741,005 inferences, 4.232 CPU in 4.238 seconds (100% CPU, 33492261 Lips)
+% 1,312,307 inferences, 0.095 CPU in 0.096 seconds (100% CPU, 13776998 Lips)
+% 2,429,967 inferences, 0.089 CPU in 0.090 seconds (100% CPU, 27168918 Lips)
+% 140,585,748 inferences, 4.440 CPU in 4.451 seconds (100% CPU, 31662188 Lips)
 % Time when doing it lazily, after refactor
 % % 939,906 inferences, 0.045 CPU in 0.046 seconds (100% CPU, 20718908 Lips)
 % % 2,782,767 inferences, 0.096 CPU in 0.096 seconds (100% CPU, 29095856 Lips)
 % % 161,753,748 inferences, 5.292 CPU in 5.302 seconds (100% CPU, 30564144 Lips)
-
-
 
 % - open('./AOC 2025/day4/test', read, _Str), read_file(_Str, _Input), ht_new(_Ht), time(day4:build_puzzle_table(_Input, _Ht)), ht_get(_Ht, (1,1), Answer).
 
