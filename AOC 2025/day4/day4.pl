06eeac9629549e38d381e67f816c1e60e0205407
mariari <mariari@protonmail.ch>
2025-12-28 00:08:48 +0800

    Removed calculating the neighbors eagerly for the keys
    Thus we remove Ele-Neighbors, and instead just have Ele, which we use to
    graph the Neighbors.
    
    We incur a speed loss of 300k, and do it by hand later but it does
    simplify the code a bit.

--- AOC 2025/day4/day4.pl
+++ AOC 2025/day4/day4.pl
@@ -1,26 +1,15 @@
 :- module(day4, [solve_day4/3, build_maze/2]).
 
-% Project a vector space into ugraphs An array here would actually
-% make it easy, I can do an APL style solution of We have to do that
-% but slow withou an equivalent of an Array neighbors([], []).
+% Project a vector space into a ugraph like relation. Done lazily, so
+% none is officially allocated.
 
 convert(64, 1). % @
 convert(46, 0). % .
 paper(64).
 
-neighbor_mapping([P | Uzzle], Neighbors) :-
-    length([P | Uzzle], Row),
-    length(P, Col),
-    Row0 is Row - 1, Col0 is Col - 1,
-    all_neighbors((Row0, Col0), Neighbors).
-
 % Could turn this into a fact to query but it isn't worth it.
-build_maze_table(Puzzle, Ht) :- ht_new(Ht), build_puzzle_table(Puzzle, Ht).
-
-build_maze(Inp, Maze) :-
-    Maze = (Neighbors, Ht),
-    neighbor_mapping(Inp, Neighbors),
-    build_maze_table(Inp, Ht).
+build_maze(Puzzle, (Ht, Keys)) :-
+    ht_new(Ht), build_puzzle_table(Puzzle, Ht), ht_keys(Ht, Keys).
 
 % Kinda an ugly way to grow this
 build_puzzle_table(Puzzle, Ht) :-
@@ -32,66 +21,66 @@ build_puzzle_table(Puzzle, Ht) :-
                 , P, 0, _))
          , Puzzle, 0, _).
 
-% This is all relations assuming infinite space, we basically run the
-% applicative ±1 but manually.
-neighbors_point((I,J), (I,J)-Rels) :-
+% table is too expensive as the predicate is cheap :- table neighboring_points/2.
+% This was a ugraph point-rels, but simplified to remove eager comp.
+neighboring_points((I,J), Rels) :-
     Rels0 = [(I-1,J-1), (I,J-1), (I+1,J-1),
              (I-1,J)  ,          (I+1,J),
              (I-1,J+1), (I,J+1), (I+1,J+1)],
     % Ugly Eval, but w/e
     maplist([(X0,Y0), (X,Y)]>>(X is X0, Y is Y0), Rels0, Rels).
 
-all_neighbors((Len_Row, Len_Col), Graph) :-
-    findall(Rel,
-            (between(0, Len_Row, Row),
-             between(0, Len_Col, Col),
-             neighbors_point((Row, Col), Rel)),
-            Graph).
-
 % Looksup the puzzle as an infinite plane
 lookup_plane(Table, Index, Value) :-
     (ht_get(Table, Index, Value) -> true; Value = 46). % Hacky default but works
 
-forkable(Table, (M-Neighbors)) :-
+forkable(Table, Roll) :-
+    neighboring_points(Roll, Neighbors),
     maplist(lookup_plane(Table), Neighbors, Numbers),
     maplist(convert, Numbers, Converted),
-    lookup_plane(Table, M, Code),
+    lookup_plane(Table, Roll, Code),
     paper(Code),
     sum_list(Converted, Sum), Sum < 4.
 
-forkable_roll((Mapping, Table), Roll) :-
-    member(Roll, Mapping), forkable(Table, Roll).
+forkable_roll((Table, Keys), Roll) :-
+    member(Roll, Keys),
+    forkable(Table, Roll).
 
-solve_part1(Maze, Result) :-
-    findall(R, forkable_roll(Maze, R), Forked), length(Forked, Result).
+all_forkable_rolls(Maze, Forked, Length) :-
+    findall(R, forkable_roll(Maze, R), Forked), length(Forked, Length).
 
 % We can make this faster, by scanning the frontier, I.E. the
 % neighbors of the removed nodes
 remove_until_stable(Maze, Total) :-
-    findall(R, forkable_roll(Maze, R), Forked),
-    ( Forked == []
+    all_forkable_rolls(Maze, Forked, N),
+    ( N = 0
     -> Total = 0
-    ; length(Forked, N),
-      remove_forked(Maze, Forked),
+    ; remove_forked(Maze, Forked),
       remove_until_stable(Maze, More),
       Total is N + More
     ).
 
-remove_forked((_Mapping, Table), Forked) :-
-    maplist({Table}/[(Ele-_)]>>(ht_put(Table, Ele, 46)), Forked).
+remove_forked((Table, _), Forked) :-
+    maplist({Table}/[Ele]>>(ht_put(Table, Ele, 46)), Forked).
 
-solve_day4(part1, Maze, Answer) :- solve_part1(Maze, Answer).
+solve_day4(part1, Maze, Answer) :- all_forkable_rolls(Maze, _, Answer).
 solve_day4(part2, Maze, Answer) :- remove_until_stable(Maze, Answer).
 
 % ?- open('./AOC 2025/day4/input', read, _Str),
 %    read_file(_Str, _Input), ht_new(_Ht),
 %    time(build_maze(_Input, _Maze)),
 %    time(solve_day4(_, _Maze, Res)).
+% Res = 1578 ;
+% Res = 10132 .
+% Time when using ugraph eagerly
 % % 1,135,450 inferences, 0.098 CPU in 0.098 seconds (100% CPU, 11641476 Lips)
 % % 2,446,404 inferences, 0.152 CPU in 0.153 seconds (100% CPU, 16050181 Lips)
-% Res = 1578 ;
 % % 141,741,005 inferences, 4.232 CPU in 4.238 seconds (100% CPU, 33492261 Lips)
-% Res = 10132 .
+% Time when doing it lazily, after refactor
+% % 939,906 inferences, 0.045 CPU in 0.046 seconds (100% CPU, 20718908 Lips)
+% % 2,782,767 inferences, 0.096 CPU in 0.096 seconds (100% CPU, 29095856 Lips)
+% % 161,753,748 inferences, 5.292 CPU in 5.302 seconds (100% CPU, 30564144 Lips)
+
 
 
 % - open('./AOC 2025/day4/test', read, _Str), read_file(_Str, _Input), ht_new(_Ht), time(day4:build_puzzle_table(_Input, _Ht)), ht_get(_Ht, (1,1), Answer).
